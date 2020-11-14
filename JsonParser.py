be2587a590313cefa5ddf87f4e47044e46d02a4f
kdvuong <khangdieuvuong@gmail.com>
2020-11-14 17:04:29 -0600

    remove comments

--- JsonParser.py
+++ JsonParser.py
@@ -5,8 +5,3 @@ class JsonParser:
         with open(fileName) as json_file:
             data = json.load(json_file)
             return data
-
-# j = JsonParser('Posts.json')
-# data = j.getData()
-# print(data)
-
