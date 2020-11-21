d72bcff40eb69ce8eae12a86af2907a952bab6c2
kdvuong <khangdieuvuong@gmail.com>
2020-11-21 15:14:02 -0600

    log score

--- phase2.py
+++ phase2.py
@@ -21,14 +21,18 @@ tags = TagController(db)
 def main():
     userId = input("Enter user id (optional): ")
     if (userId != ""):
-        questions = posts.getQuestions(userId)
-        questionsCount = questions.count()
+        questions = list(posts.getQuestions(userId))
+        questionsCount = len(questions)
         totalScore = 0
         for question in questions:
             totalScore += question["Score"]
+
+        avgScore = 0
+        if (questionsCount > 0):
+            avgScore = totalScore/questionsCount
         
         print("Questions count: {count}".format(count = questionsCount))
-        print("Avg score      : {score}".format(score = totalScore/questionsCount))
+        print("Avg score      : {score}".format(score = avgScore))
         
         print("DONE DONE DONE DONE------------")
     else:
